b7f65aa42f7deb5b1866dbbc9eb714959a821266
SXsid <sidshekhar2024@gmail.com>
2026-02-25 03:00:26 +0530

    reafctore http server

--- backend-performance/server/src/main.c
+++ backend-performance/server/src/main.c
@@ -1,27 +1,30 @@
-
 #include <netinet/in.h> //http protocla and config
+#include <signal.h>
 #include <stdio.h>      //for input out
 #include <stdlib.h>     // funcion to handle meroy and core funtin
 #include <string.h>     //all operation on char* DS
 #include <sys/socket.h> //all the server / netowrk warpper
 #include <unistd.h>     //sys call
 
-
 #define PORT 8080
 #define MAX_REQUEST_SIZE 8192
 
-
+volatile sig_atomic_t keep_running = 1;
+void handlesignal() {
+        keep_running = 0;
+}
 int handleConnection(int clietn_fd) {
         char buffer[MAX_REQUEST_SIZE];
         size_t bytes_read = 0;
         while (1) {
                 int n = read(clietn_fd, buffer + bytes_read, sizeof(buffer) - bytes_read);
                 if (n < 0) {
+                        if (!keep_running)
+                                break;
                         perror("read");
                         return -1;
                 }
                 if (n == 0) {
-                        // client closed
                         break;
                 }
                 bytes_read += n;
@@ -35,19 +38,33 @@ int handleConnection(int clietn_fd) {
                 }
         }
         printf("%.*s", (int)bytes_read, buffer);
+        char *http_response = "HTTP/1.1 200 OK\r\n"
+                              "Content-Type: text/plain\r\n"
+                              "Content-Length: 12\r\n"
+                              "Connection: close\r\n"
+                              "\r\n"
+                              "Hello world!";
+        int wn = write(clietn_fd, http_response, strlen(http_response));
+        if (wn < 0) {
+                perror("write");
+                return -1;
+        }
         return 0;
 }
 
-
 int main() {
-        // create a socket
+        signal(SIGINT, handlesignal);
+        signal(SIGTERM, handlesignal);
         int server_fd = socket(AF_INET, SOCK_STREAM, 0);
         if (server_fd == 0) {
                 perror("Socket Failed");
                 exit(EXIT_FAILURE);
         };
-
-        // bind a socket
+        int opt = 1;
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+                perror("setsockopt");
+                exit(EXIT_FAILURE);
+        }
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(PORT);
@@ -56,17 +73,18 @@ int main() {
                 perror("bind");
                 exit(EXIT_FAILURE);
         }
-        // now we have synce que and accept que(client who has sended ack)
         if (listen(server_fd, 10) < 0) {
                 perror("listen");
                 exit(EXIT_FAILURE);
         }
         printf("we are waiting for a connection \n");
-        while (1) {
+        while (keep_running) {
                 struct sockaddr_in client_add;
                 socklen_t client_addr_len = sizeof(client_add);
                 int clientFd = accept(server_fd, (struct sockaddr *)&client_add, &client_addr_len);
                 if (clientFd < 0) {
+                        if (!keep_running)
+                                break;
                         perror("accept");
                         continue;
                 }
@@ -74,9 +92,11 @@ int main() {
                 if (handleConnection(clientFd) < 0) {
                         perror("Error handling client");
                         close(clientFd);
+                        printf("Conecction closed%d\n", client_add.sin_port);
                         continue;
                 }
                 close(clientFd);
+                printf("Conecction closed%d\n", client_add.sin_port);
         }
         close(server_fd);
 }
