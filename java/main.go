6ec5331b1a58b7c8b89eee4054b83a321a8020ba
Sudhanshu Shekhar <sidshekhar2024@gmail.com>
2026-05-30 13:06:07 +0530

    till abastraction and interface

---
+++ java/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Institute struct{}
+
+func (i Institute) Teach() {
+	fmt.Println("Teaches")
+}
+
+type College struct {
+	Institute
+}
+
+func (c College) Sports() {
+	fmt.Println("College Sports")
+}
+
+func (c College) Reserach() {
+	fmt.Println("Research college")
+}
+
+// compositotn over inherticae  abstraic achive by iinteface
+type Uni struct {
+	College
+}
+
+func (u Uni) Reserach() {
+	fmt.Println("uni Research college")
+}
+
+func main() {
+	uni := Uni{}
+	uni.Teach()
+	uni.Reserach()
+	uni.Sports()
+}
