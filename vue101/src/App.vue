ba3c8bbecd25dc8e089de4a5f09abcc560daca19
Sudhanshu Shekhar <sidshekhar2024@gmail.com>
2026-07-28 15:30:18 +0530

    init

---
+++ vue101/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const name ="Harsh"
+import HelloWorld from './components/HelloWorld.vue'
+const AlertValue=(count)=>{
+
+
+    alert("current value is "+count)
+
+}
+</script>
+
+<template>
+  <HelloWorld :name="name" v-on:countClicked="AlertValue" />
+</template>
